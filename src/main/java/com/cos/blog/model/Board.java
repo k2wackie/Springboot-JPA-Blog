7db8132c7a58fe9692364c3ef87bb6d580376908
Kyle KIM <k2wackie@gmail.com>
2022-05-18 14:01:00 +0900

    연관관계 수정

--- src/main/java/com/cos/blog/model/Board.java
+++ src/main/java/com/cos/blog/model/Board.java
@@ -1,15 +1,18 @@
 package com.cos.blog.model;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
@@ -39,10 +42,15 @@ public class Board {
 	@ColumnDefault("0")
 	private int count; // 조회수
 
-	@ManyToOne // Many=Board, User = One 연관관계
+	@ManyToOne(fetch = FetchType.EAGER) // Many=Board, User = One 연관관계 //EAGER:바로 불러오기, LAZY:필요시 불러오기
 	@JoinColumn(name = "userId")
-	private User userId; // DB는 오브젝트를 저장할 수 없음. FK(foreign key), 자바는 오브젝트를 저장할 수 있음, ORM을 사용하면 오브젝트를 저장
-							// 가능
+	private User user; // DB는 오브젝트를 저장할 수 없음. FK(foreign key), 자바는 오브젝트를 저장할 수 있음, ORM을 사용하면 오브젝트를 저장
+						// 가능
+
+	// ManyToOne:기본 EAGER, OneToMany:기본 LAZY
+
+	@OneToMany(mappedBy = "board", fetch = FetchType.EAGER) // mappedBy 연관관계의 주인이 아니다 (난 FK가 아니니) DB에 칼럼을 만들지 않도록
+	private List<Reply> reply;
 
 	@CreationTimestamp
 	private Timestamp createDate;
