ec6e4c6b0baeafb113d11476ccf054d4e872cfc3
Kyle KIM <k2wackie@gmail.com>
2022-05-20 11:14:46 +0900

    스프링 시큐리티 적용

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -1,7 +1,5 @@
 package com.cos.blog.controller.api;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,9 +17,6 @@ public class UserApiController {
 	@Autowired
 	private UserService userService;
 
-	@Autowired
-	private HttpSession session;
-
 	@PostMapping("/api/user")
 	public ResponseDto<Integer> save(@RequestBody User user) {
 		System.out.println("UserApiController: save 호출됨");
@@ -30,14 +25,15 @@ public class UserApiController {
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), result);
 	}
 
-	// 전통적인 로그인방식
-	@PostMapping("/api/user/login")
-	public ResponseDto<Integer> login(@RequestBody User user) {
-		System.out.println("UserApiController: login 호출됨");
-		User principal = userService.로그인(user); // principal(접근주체)
-		if (principal != null) {
-			session.setAttribute("principal", principal);
-		}
-		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
-	}
+//	// 전통적인 로그인방식
+//	@PostMapping("/api/user/login")
+//	public ResponseDto<Integer> login(@RequestBody User user, HttpSession session) {
+//		System.out.println("UserApiController: login 호출됨");
+//		User principal = userService.로그인(user); // principal(접근주체)
+//		if (principal != null) {
+//			session.setAttribute("principal", principal);
+//		}
+//		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+//	}
+
 }
