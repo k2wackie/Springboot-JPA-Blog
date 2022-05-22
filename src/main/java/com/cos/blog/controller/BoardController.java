7b448a74d71475f90dbdbe4bcf44e57abdab063a
Kyle KIM <k2wackie@gmail.com>
2022-05-22 15:43:36 +0900

    글목록 페이징

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,6 +1,9 @@
 package com.cos.blog.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,11 +15,11 @@ public class BoardController {
 
 	@Autowired
 	private BoardService boardService;
-	
+
 	@GetMapping({ "", "/" })
-	public String index(Model model) {
-		model.addAttribute("boards", boardService.글목록());
-				
+	public String index(Model model, @PageableDefault(size = 3, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+		model.addAttribute("boards", boardService.글목록(pageable));
+
 		// /WEB-INF/views/index.jsp
 		return "index"; // viewResolver 작동
 	}
--- src/main/java/com/cos/blog/test/DummyController.java
+++ src/main/java/com/cos/blog/test/DummyController.java
@@ -75,10 +75,11 @@ public class DummyController {
 
 	// 한페이지당 2 건의 데이터를 리턴
 	@GetMapping("/dummy/user")
-	public List<User> pageList(@PageableDefault(size = 2, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+	public Page<User> pageList(@PageableDefault(size = 2, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
 		Page<User> pagingUsers = userRepository.findAll(pageable);
+		
 		List<User> users = pagingUsers.getContent();
-		return users;
+		return pagingUsers;
 	}
 
 	// {id}주소로 파라미터를 전달 받을 수 있음
