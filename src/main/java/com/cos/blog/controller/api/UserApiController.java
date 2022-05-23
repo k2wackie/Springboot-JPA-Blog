f11586cad74fd6685e55f19c135dac0ece462cac
Kyle KIM <k2wackie@gmail.com>
2022-05-23 13:26:38 +0900

    회원 수정2

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -1,12 +1,21 @@
 package com.cos.blog.controller.api;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cos.blog.config.auth.PrincipalDetail;
 import com.cos.blog.dto.ResponseDto;
 import com.cos.blog.model.User;
 import com.cos.blog.service.UserService;
@@ -17,6 +26,9 @@ public class UserApiController {
 	@Autowired
 	private UserService userService;
 
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
 	@PostMapping("/auth/joinProc")
 	public ResponseDto<Integer> save(@RequestBody User user) {
 		System.out.println("UserApiController: save 호출됨");
@@ -27,7 +39,21 @@ public class UserApiController {
 
 	@PutMapping("/user")
 	public ResponseDto<Integer> update(@RequestBody User user) {  //@RequestBody가 없으면 json형태의 데이터가 아닌 key=value 형태를 받음
+//	public ResponseDto<Integer> update(@RequestBody User user, @AuthenticationPrincipal PrincipalDetail principal, HttpSession session) {  //@RequestBody가 없으면 json형태의 데이터가 아닌 key=value 형태를 받음
 		userService.회원수정(user);
+		// 여기서는 트랜잭션이 종료되기 때문에 DB에 값은 변경이 됐지만
+		// 세션값은 변경되지 않은 상태이기 때문에 우리가 직접 세션값을 변경해 줘야함 -> 구현 안됨...
+//		Authentication authentication = new UsernamePasswordAuthenticationToken(principal, null);
+//		SecurityContext securityContext = SecurityContextHolder.getContext();
+//		securityContext.setAuthentication(authentication); 
+//		session.setAttribute("SPRING_SECURITY_CONTEXT", securityContext);
+		//구현 안 됨..
+		
+		
+		//세션 등록
+		Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
 }
--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -38,6 +38,7 @@ const index = {
 		update: function() {
 		const data = {
 			id: $("#id").val(),
+			username: $("#username").val(),
 			password: $("#password").val(),
 			email: $("#email").val(),
 		};
